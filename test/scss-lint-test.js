56c80f327904d0fb567e9a044b597738dc4b7bcf
Ahmed Nuaman <ahmed@ahmednuaman.com>
2013-12-01 16:08:28 +0000

    more debugging

--- test/scss-lint-test.js
+++ test/scss-lint-test.js
@@ -28,6 +28,7 @@ exports.scsslint = {
     test.expect(3);
     var files = path.join(fixtures, 'fail.scss');
     scsslint.lint(files, options, function(results) {
+      console.log(results);
       test.ok(results[0].indexOf('Class `Button` in selector should be written in all lowercase as `button`') !== -1, 'Should report bad case.');
       test.ok(results[1].indexOf('Properties should be sorted in alphabetical order, with vendor-prefixed extensions before the standardized CSS property') !== -1, 'Should report bad ordering.');
       test.ok(results[2].indexOf('Color `black` should be written in hexadecimal form as `#000`') !== -1, 'Should report string colour usage.');
@@ -40,6 +41,7 @@ exports.scsslint = {
     test.expect(1);
     var files = path.join(fixtures, 'pass.scss');
     scsslint.lint(files, options, function(results) {
+      console.log(results);
       test.ok(results[0] === '', 'There should be no lint errors');
       test.done();
     });
@@ -50,6 +52,7 @@ exports.scsslint = {
     test.expect(1);
     var files = path.join(fixtures, 'pass.scss');
     scsslint.lint([files, files, files], options, function(results) {
+      console.log(results);
       test.ok(results[0] === '', 'There should be no lint errors');
       test.done();
     });
@@ -60,6 +63,7 @@ exports.scsslint = {
     test.expect(1);
     var files = path.join(fixtures, 'fail.scss');
     scsslint.lint(files, {reporterOutput: reporterOutFile}, function(results) {
+      console.log(results);
       var report = grunt.file.read(reporterOutFile);
 
       test.ok(report.indexOf(results[0]) !== -1, 'Should write the errors out to a report');

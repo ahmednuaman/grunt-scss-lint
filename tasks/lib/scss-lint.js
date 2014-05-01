95107c9204ee55fe71b2428aae9e5226134deb5a
Mike Sidorov <mikes.ekb@gmail.com>
2014-05-01 10:51:53 +0600

    Allow user to suppress colorized output by setting only 1 option to false, not both.

--- tasks/lib/scss-lint.js
+++ tasks/lib/scss-lint.js
@@ -76,7 +76,7 @@ exports.init = function (grunt) {
       args.push(grunt.file.expand(exclude).join(','));
     }
 
-    if (options.colorizeOutput || options.colouriseOutput) {
+    if (options.colorizeOutput && options.colouriseOutput) {
       env.CLICOLOR_FORCE = '1';
     }
 

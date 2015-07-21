5e0499e740cdeb47f922d960c0e3f513f0c90a61
Nick Schonning <nschonni@gmail.com>
2015-07-20 22:49:06 -0400

    Use binary checking from Grunt-Contrib-Sass

--- tasks/scss-lint.js
+++ tasks/scss-lint.js
@@ -1,6 +1,17 @@
 module.exports = function (grunt) {
   var _ = require('lodash'),
-      scsslint = require('./lib/scss-lint').init(grunt);
+      scsslint = require('./lib/scss-lint').init(grunt),
+      which = require('which'),
+      checkBinary = function (cmd, errMsg) {
+        try {
+          which.sync(cmd);
+        } catch (err) {
+          return grunt.warn(
+            '\n' + errMsg + '\n' +
+            'More info: https://github.com/ahmednuaman/grunt-scss-lint\n'
+          );
+        }
+      };
 
   grunt.registerMultiTask('scsslint', 'Validate `.scss` files with `scss-lint`.', function () {
     var done = this.async(),
@@ -21,6 +32,16 @@ module.exports = function (grunt) {
       force: false,
       maxBuffer: 300 * 1024
     });
+
+    if (opts.bundleExec) {
+      checkBinary('bundle',
+        'bundleExec options set but no Bundler executable found in your PATH.'
+      );
+    } else {
+      checkBinary('scss-lint',
+        'You need to have Ruby and sscs_lint installed and in your PATH for this task to work.'
+      );
+    }
 
     grunt.verbose.writeflags(opts, 'scss-lint options');
     grunt.log.writeln('Running scss-lint on ' + target);
